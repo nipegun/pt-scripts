bca11620438127ccdd2a19c528c66f6bb3c0ee0c
NiPeGun <nipegun@gmail.com>
2025-02-07 01:32:23 +0100

    Update DNS-Cache-Spoofing.sh

--- Ataques/DNS-Cache-Spoofing.sh
+++ Ataques/DNS-Cache-Spoofing.sh
@@ -1,10 +1,11 @@
-# Preguntar al servidor raíz a.root-servers.net
+# Preguntar al servidor raíz
   vRespServRaiz=$(dig @a.root-servers.net openwrt.org NS +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
+  echo "  El servidor raíz ha mandando a consultar a $vRespServRaiz."
 
-# Preguntar al servidor del TLD .org a0.org.afilias-nst.info
+# Preguntar al servidor TLD obtenido de la consulta anterior
   vRespServTLD=$(dig @$vRespServRaiz openwrt.org NS +norecurse | grep -P "IN\tNS\t" | sort | sed -e 's-\t- -g' | head -n1 | cut -d' ' -f6 | sed 's/.$//')
+  echo "  El servidor TLD ha mandado a consultar a $vRespServTLD."
 
-# Preguntar al servidor autoritativo de openwrt.org	ns1.openwrt.org
+# Preguntar al servidor autoritativo obtenido en la consulta anterior
   vRespServAuth=$(dig @$vRespServTLD openwrt.org A +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f6)
-
-echo $vRespServAuth
+  echo "  El servidor autoritativo ha repondido que la IP de dominio es $vRespServAuth"
