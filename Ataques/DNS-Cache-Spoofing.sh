1d33dbc3c3f1573369abd616dacc5f4649ecb219
NiPeGun <nipegun@gmail.com>
2025-02-07 01:19:15 +0100

    Update DNS-Cache-Spoofing.sh

--- Ataques/DNS-Cache-Spoofing.sh
+++ Ataques/DNS-Cache-Spoofing.sh
@@ -2,9 +2,9 @@
   vRespServRaiz=$(dig @a.root-servers.net openwrt.org NS +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
 
 # Preguntar al servidor del TLD .org a0.org.afilias-nst.info
-  vRespServTLD=$(dig @$vIPServRaiz openwrt.org NS +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
+  vRespServTLD=$(dig @$vRespServRaiz openwrt.org NS +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
 
 # Preguntar al servidor autoritativo de openwrt.org	ns1.openwrt.org
-  vRespServAuth=$(dig @$vIPServTLD openwrt.org A +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
+  vRespServAuth=$(dig @$vRespServTLD openwrt.org A +norecurse | grep -P "IN\tA\t" | sort | head -n1 | sed -e 's-\t- -g' | cut -d' ' -f5)
 
 echo $vRespServAuth
