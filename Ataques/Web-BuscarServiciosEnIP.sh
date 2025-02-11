ccdb001ff4205f246e10ae5d5d1100701cce6a71
NiPeGun <nipegun@gmail.com>
2025-02-11 10:12:34 +0100

    Update Web-BuscarServiciosEnIP.sh

--- Ataques/Web-BuscarServiciosEnIP.sh
+++ Ataques/Web-BuscarServiciosEnIP.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
 vHost="localhost"
 
 nmap "$vHost" -p- | sed "s|^|http://$vHost:|" | sed 's-/tcp--g'
