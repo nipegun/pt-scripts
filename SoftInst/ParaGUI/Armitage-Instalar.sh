8861d04942f10a1717377ba38a9faac5c798ada3
NiPeGun <nipegun@gmail.com>
2025-02-18 17:15:19 +0100

    Update Armitage-Instalar.sh

--- SoftInst/ParaGUI/Armitage-Instalar.sh
+++ SoftInst/ParaGUI/Armitage-Instalar.sh
@@ -9,16 +9,10 @@
 # Script de NiPeGun para instalar y configurar Armitage en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | bash -s IPDeDescarga
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | sed 's-sudo--g' | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | sed 's-sudo--g' | bash -s IPDeDescarga
 #
 # Bajar y editar directamente el archivo en nano
 #   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ParaGUI/Armitage-Instalar.sh | nano -
@@ -33,6 +27,25 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
+# Definir la cantidad de argumentos esperados
+  cCantParamEsperados=2
+
+# Comprobar que se hayan pasado la cantidad de parámetros correctos. Abortar el script si no.
+  if [ $# -ne $cCantParamEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo "    $0 [Parámetro1] [Parámetro2]"
+      echo ""
+      echo "  Ejemplo:"
+      echo "    $0 'Hola' 'Mundo'"
+      echo ""
+      exit
+  fi
+
+# Guardar el primer parámetro como URL
+  vURLArchivo="$1"
+
 # Comprobar si el script está corriendo como root
   #if [ $(id -u) -ne 0 ]; then     # Sólo comprueba si es root
   if [[ $EUID -ne 0 ]]; then       # Comprueba si es root o sudo
@@ -40,16 +53,6 @@
     echo -e "${cColorRojo}  Este script está preparado para ejecutarse con privilegios de administrador (como root o con sudo).${cFinColor}"
     echo ""
     exit
-  fi
-
-# Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
-  if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
-    echo ""
-    sudo apt-get -y update
-    sudo apt-get -y install curl
-    echo ""
   fi
 
 # Determinar la versión de Debian
@@ -90,22 +93,89 @@
     echo -e "${cColorAzulClaro}  Iniciando el script de instalación de Armitage para Debian 12 (Bookworm)...${cFinColor}"
     echo ""
 
-      echo "    Recuerda iniciar el servidor msfrpcd para poder conectar armitage a Metasploit Framework:"
+    # Descargar el archivo .tar.gz
       echo ""
-      echo "      msfrpcd -U msf -P P@ssw0rd"
+      echo "    Descargando el archivo .tar.gz"
       echo ""
-      echo "        o, con SSL:"
+      # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install curl
+          echo ""
+        fi
+      curl -L "$vURLArchivo" -o /tmp/armitage.tar.gz
+
+    # Descomprimir arhivo
       echo ""
-      echo "      msfrpcd -U msf -P P@ssw0rd --ssl"
+      echo "    Descomprimiendo archivo..."
       echo ""
-      echo "      También puedes iniciarlo desde la propia consola de mft:"
+      cd /tmp
+      sudo rm -rf /tmp/armitage/*
+      sudo rm -f /tmp/armitage
+      mkdir /tmp/armitage/
+      # Comprobar si el paquete tar está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s tar 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${cColorRojo}  El paquete tar no está instalado. Iniciando su instalación...${cFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install tar
+          echo ""
+        fi
+      tar -xvzf /tmp/armitage.tar.gz -C /tmp/armitage
+      rm -f /tmp/armitage/ArmitageLinux.tar.gz 2> /dev/null
+
+    # Instalar en la carpeta aconsejada
       echo ""
-      echo "        sudo msfconsole"
+      echo "  Instalando en la carpeta aconsejada"
       echo ""
-      echo "          load msgrpc Pass=P@ssw0rd"
+      sudo rm -rf /opt/armitage
+      sudo mv /tmp/armitage/ /opt/
+      sudo chown $USER:$USER /opt/armitage -R
+
+    # Crear el icono para lanzar la aplicación desde el menú gráfico
       echo ""
-      sudo pkill -f msf
-      sudo systemctl restart postgresql
+      echo "    Creando el lanzador gráfico..."
+      echo ""
+      mkdir -p ~/.local/share/applications
+      echo '[Desktop Entry]'                      | sudo tee    ~/.local/share/applications/Armitage.desktop
+      echo 'Name=Armitage'                        | sudo tee -a ~/.local/share/applications/Armitage.desktop
+      echo 'Categories=Development'               | sudo tee -a ~/.local/share/applications/Armitage.desktop
+      echo "Exec=/opt/armitage/armitage"          | sudo tee -a ~/.local/share/applications/Armitage.desktop
+      echo "Icon=/opt/armitage/armitage-logo.png" | sudo tee -a ~/.local/share/applications/Armitage.desktop
+      echo 'Type=Application'                     | sudo tee -a ~/.local/share/applications/Armitage.desktop
+      echo 'Terminal=false'                       | sudo tee -a ~/.local/share/applications/Armitage.desktop
+
+    # Modificar las variables de entorno
+      # Determinar si la base de datos de metasploit existe
+        if [ -f $HOME/.msf4/database.yml ]; then
+          echo "  El archivo de base de datos de Metasplot Framework existe."
+          echo "  Agregándolo como variable de entorno al script..."
+          sudo sed -i -e "s|cd /opt/armitage/|cd /opt/armitage/\nexport MSF_DATABASE_CONFIG=$HOME/.msf4/database.yml|g" /opt/armitage/armitage
+        else
+          echo "  El archivo de base de datos de Metasplot Framework no existe."
+          echo "  No se agregará una variable de entorno al script."
+        fi
+
+    # Notificar fin de ejecución del script
+      echo ""
+      echo "  El script de instalación de armitage ha finalizado."
+      echo ""
+      echo "    Para iniciar el servidor msfrpcd desde bash:"
+      echo ""
+      echo "      msfrpcd -U msf -P P@ssw0rd -f -S -a 127.0.0.1"
+      echo ""
+      echo "      Luego lanza la aplicación gráfica de Armitage y conectate al servidor local:"
+      echo ""
+      echo "    También puedes iniciar el servidor rpc desde la propia consola de metasploit:"
+      echo ""
+      echo "      msfconsole"
+      echo ""
+      echo "        load msgrpc Pass=P@ssw0rd"
+      echo ""
 
   elif [ $cVerSO == "11" ]; then
 
