44686ab69507f6a1c74f65ec304f4af5360c405e
NiPeGun <nipegun@gmail.com>
2025-02-19 08:13:08 +0100

    Update and rename SoftInst/ServWeb/WebGoat7-Instalar.sh to SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh

--- SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh
+++ SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar WebGoat7 en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/WebGoat7-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/WebGoat7-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/WebGoat7-Instalar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/WebGoat7-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
