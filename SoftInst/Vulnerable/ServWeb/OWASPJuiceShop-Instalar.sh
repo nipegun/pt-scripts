7886d62d029560f0177ec27d8f212c24c63227bb
NiPeGun <nipegun@gmail.com>
2025-02-19 15:54:50 +0100

    Update OWASPJuiceShop-Instalar.sh

--- SoftInst/Vulnerable/ServWeb/OWASPJuiceShop-Instalar.sh
+++ SoftInst/Vulnerable/ServWeb/OWASPJuiceShop-Instalar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar OWASP Juice Shop en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/OWASPJuiceShop-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/Vulnerable/ServWeb/OWASPJuiceShop-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/OWASPJuiceShop-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/Vulnerable/ServWeb/OWASPJuiceShop-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/ServWeb/Vulnerable/OWASPJuiceShop-Instalar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/SoftInst/Vulnerable/ServWeb/OWASPJuiceShop-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
