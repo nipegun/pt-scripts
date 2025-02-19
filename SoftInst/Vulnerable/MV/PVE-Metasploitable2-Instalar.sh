fb94edeb05cb2ef04b01c348ac2431f418747c69
NiPeGun <nipegun@gmail.com>
2025-02-19 16:40:40 +0100

    Create PVE-Metasploitable2-Instalar.sh

---
+++ SoftInst/Vulnerable/MV/PVE-Metasploitable2-Instalar.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para crear un laboratorio de ciberseguridad en Proxmox
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/p-scripts/refs/heads/master/MVs/Packs/Lab-CyberSec-Importar.sh | bash
+#
+# Ejecución remota con parámetros:
+#   curl sL https://raw.githubusercontent.com/nipegun/p-scripts/refs/heads/master/MVs/Packs/Lab-CyberSec-Importar.sh | bash -s Almacenamiento
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/p-scripts/refs/heads/master/MVs/Packs/Lab-CyberSec-Importar.sh | nano -
+# ----------
+
+vLinkDeDescarga="https://sourceforge.net/projects/metasploitable/files/latest/download"
+vAlmacenamiento="local-lvm"
+vIDdeLaMV=2000
+vIDDelPuente="vmbr80"
+vDirMac="00:aa:aa:aa:10:02"
+
+# Crear la máquina virtual
+  echo ""
+  echo "  Creando la máquina virtual de Metasploitable 2..."
+  echo ""
+  qm create $vIDdeLaMV \
+    --name metasploitable2 \
+    --machine q35 \
+    --numa 0 \
+    --sockets 1 \
+    --cpu x86-64-v2-AES \
+    --cores 4 \
+    --memory 4096 \
+    --balloon 0 \
+    --vga memory=512 \
+    --net0 virtio=$vDirMac,bridge=$vIDDelPuente,firewall=1 \
+    --boot order=sata0 \
+    --scsihw virtio-scsi-single \
+    --sata0 none,media=cdrom \
+    --ostype l26 \
+    --agent 1
+
+# Descargar el archivo
+  # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install curl
+      echo ""
+    fi
+  curl -L "$vLinkDeDescarga" -o /tmp/Metasploitable2.zip
+
+# Descomprimir el archivo
+  # Comprobar si el paquete zip está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s zip 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete zip no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install zip
+      echo ""
+    fi
+  cd /tmp/
+  unzip /tmp/Metasploitable2.zip
+
+# Importar el archivo .vmdk a la máquina virtual
+  echo ""
+  echo "    Importando el archivo de .vmdk a la máquina virtual..."
+  echo ""
+  qm importdisk $vIDdeLaMV /tmp/Metasploitable2-Linux/Metasploitable.vmdk "$vAlmacenamiento" && rm -f /tmp/Metasploitable2-Linux/Metasploitable.vmdk
+  vRutaAlDisco=$(qm config $vIDdeLaMV | grep unused | cut -d' ' -f2)
+  qm set $vIDdeLaMV --sata1 $vRutaAlDisco
+  qm set $vIDdeLaMV --boot order='sata0;sata1'
