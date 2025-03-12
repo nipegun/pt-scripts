0a788ffa361ccef7c2a91840f8da4caf57286114
NiPeGun <nipegun@gmail.com>
2025-03-12 13:55:28 +0100

    Update OpenStego-Instalar.sh

--- SoftInst/ParaGUI/OpenStego-Instalar.sh
+++ SoftInst/ParaGUI/OpenStego-Instalar.sh
@@ -6,6 +6,7 @@
   vUltVers=$(curl -s https://api.github.com/repos/syvaidya/openstego/releases/latest | jq -r '.tag_name')
 
 # Buscar el enlace 
+  curl -sL https://github.com/syvaidya/openstego/releases/tag/$vUltVers
 
 # Descargar el .deb
   curl -sL https://github.com/syvaidya/openstego/releases/download/$vUltVers/openstego_0.8.6-1_all.deb -o /tmp/openstego.deb
