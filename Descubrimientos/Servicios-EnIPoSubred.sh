52e9aa03c81e3120a570821eefad1f587d7fdfff
NiPeGun <nipegun@gmail.com>
2025-03-26 03:31:48 +0100

    Update Servicios-EnIPoSubred.sh

--- Descubrimientos/Servicios-EnIPoSubred.sh
+++ Descubrimientos/Servicios-EnIPoSubred.sh
@@ -27,56 +27,6 @@
   echo -e "${cColorAzulClaro}  Buscando servicios en $vIPoSubred ... ${cFinColor}"
   echo ""
 
-# Ejecutar Nmap y extraer los números de puerto
-  mapfile -t vPuertosConRespuesta < <(nmap -p- "$vIPoSubred" | grep -oP '^\d+(?=/)')
+# Ejecutar nmap
+  nmap "$vIPoSubred" -Pn -p- -sV | sed -e 's-\\x20- -g' | sed -e 's-\\r-\n-g' | sed -e 's-\\n-\n-g' | sed -e 's-SF:--g'
 
-# Verificar si se encontraron puertos
-  if [ ${#vPuertosConRespuesta[@]} -eq 0 ]; then
-    echo -e "${cColorRojo}    No se encontraron puertos abiertos en $vIPoSubred ${cFinColor}"
-    echo ""
-    exit 1
-  fi
-
-# Notificar todos los puertos abiertos que se encontraron
-  echo -e "${cColorVerde}    Se encontraron ${#vPuertosConRespuesta[@]} puertos abiertos. ${cFinColor}"
-  echo ""
-
-# Array para puertos con respuesta HTML
-  vPuertosConRespuestaHTML=()
-
-# Iterar sobre los puertos y probar con curl en HTTP y HTTPS
-  for puerto in "${vPuertosConRespuesta[@]}"; do
-    echo ""
-    echo "  Probando HTTP en puerto $puerto..."
-    if curl -s --max-time 3 "http://$vIPoSubred:$puerto" | grep -q "<html"; then
-      echo -e "${cColorVerde}    Respuesta HTML detectada en http://$vIPoSubred:$puerto ${cFinColor}"
-      vPuertosConRespuestaHTML+=("http://$vIPoSubred:$puerto")
-    else
-      echo -e "${cColorRojo}    No se detectó HTML en http://$vIPoSubred:$puerto. ${cFinColor}"
-    fi
-
-    echo ""
-    echo "  Probando HTTPS en puerto $puerto..."
-    if curl -s --max-time 3 -k "https://$vIPoSubred:$puerto" | grep -q "<html"; then
-      echo -e "${cColorVerde}    Respuesta HTML detectada en https://$vIPoSubred:$puerto ${cFinColor}"
-      vPuertosConRespuestaHTML+=("https://$vIPoSubred:$puerto")
-    else
-      echo -e "${cColorRojo}    No se detectó respuesta HTML en https://$vIPoSubred:$puerto ${cFinColor}"
-    fi
-  done
-
-# Mostrar los puertos que devolvieron HTML, línea por línea
-  if [ ${#vPuertosConRespuestaHTML[@]} -gt 0 ]; then
-    echo ""
-    echo "  Resultado:"
-    echo ""
-    for vURL in "${vPuertosConRespuestaHTML[@]}"; do
-      echo -e "${cColorVerde}    $vURL ${cFinColor}"
-    done
-  else
-    echo ""
-    echo "  Resultado:"
-    echo ""
-    echo -e "${cColorRojo}    No se encontraron puertos con respuesta HTML en $vIPoSubred ${cFinColor}"
-  fi
-  echo ""
