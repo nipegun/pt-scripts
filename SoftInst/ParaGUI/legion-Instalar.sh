03df2be0257c91e3ce31e77018f3501733f905b4
NiPeGun <nipegun@gmail.com>
2025-03-30 00:26:07 +0100

    Update legion-Instalar.sh

--- SoftInst/ParaGUI/legion-Instalar.sh
+++ SoftInst/ParaGUI/legion-Instalar.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+git clone https://github.com/Hackman238/legion.git ~/HackingTools/Legion
+cd ~/HackingTools/Legion
+sudo ~/HackingTools/Legion/deps/installDeps.sh
+eyewitness
+phantomjs
+sudo ~/HackingTools/Legion/deps/installPythonLibs.sh
+cat requirements.txt | xargs sudo pip3 install --break-system-packages 
+cp ~/HackingTools/Legion/app/importers/NmapImporter.py ~/HackingTools/Legion/app/importers/NmapImporter.py.bak 
+
+echo ""
+echo "  Legion instalado. Ejecutalo con:"
+echo ""
+echo "    sudo python3 legion.py"
+echo ""
+
