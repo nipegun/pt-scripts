2718cd2d7bbfd7b66792fc885f99f2187b8128a7
NiPeGun <nipegun@gmail.com>
2025-03-30 00:45:37 +0100

    Update legion-Instalar.sh

--- SoftInst/ParaGUI/legion-Instalar.sh
+++ SoftInst/ParaGUI/legion-Instalar.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-git clone https://github.com/Hackman238/legion.git ~/HackingTools/Legion
-cd ~/HackingTools/Legion
-sudo ~/HackingTools/Legion/deps/installDeps.sh
-eyewitness
-phantomjs
-sudo ~/HackingTools/Legion/deps/installPythonLibs.sh
-cat requirements.txt | xargs sudo pip3 install --break-system-packages 
-cp ~/HackingTools/Legion/app/importers/NmapImporter.py ~/HackingTools/Legion/app/importers/NmapImporter.py.bak 
+# Instalar en venv
+  cd ~/
+  sudo rm -rf ~/HackingTools/Legion/
+  git clone https://github.com/Hackman238/legion.git ~/HackingTools/Legion
+  sudo ~/HackingTools/Legion/deps/installDeps.sh
+  eyewitness
+  phantomjs
+  cd ~/HackingTools/Legion/
+  python3 -m venv venv
+  source ~/HackingTools/Legion/venv/bin/activate
+  ~/HackingTools/Legion/deps/installPythonLibs.sh
+
+  
+  chmod +x legion.py
+  sudo ~/HackingTools/Legion/legion.py
+  
+# Instalar a nivel de sistema
+  cat requirements.txt | xargs sudo pip3 install --break-system-packages 
+  cp ~/HackingTools/Legion/app/importers/NmapImporter.py ~/HackingTools/Legion/app/importers/NmapImporter.py.bak 
 
 echo ""
 echo "  Legion instalado. Ejecutalo con:"
@@ -15,3 +26,4 @@ echo ""
 echo "    sudo python3 legion.py"
 echo ""
 
+# Instalar en
