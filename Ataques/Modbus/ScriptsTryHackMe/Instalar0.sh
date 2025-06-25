e74005e80b42acb8c7e0dd1286968d01dec7b394
NiPeGun <nipegun@gmail.com>
2025-06-26 00:30:43 +0200

    Update and rename Instalar.sh to Instalar0.sh

--- Ataques/Modbus/ScriptsTryHackMe/Instalar0.sh
+++ Ataques/Modbus/ScriptsTryHackMe/Instalar0.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+# Script de NiPeGun para instalar y configurar los scripts de Modbus de TryHackMe en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
 #   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Ataques/Modbus/ScriptsTryHackMe/Instalar.sh | bash
