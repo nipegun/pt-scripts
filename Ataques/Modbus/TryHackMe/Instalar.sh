f196987c1fb18cb3a805d6e455437b3cb4ae28a8
NiPeGun <nipegun@gmail.com>
2025-06-26 00:11:44 +0200

    Create Instalar.sh

---
+++ Ataques/Modbus/TryHackMe/Instalar.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+# Crear la carpeta HackingTools
+  mkdir -p $HOME/HackingTools/ 2> /dev/null
+
+# Crear la carpeta para estos scripts
+  mkdir -p $HOME/HackingTools/Modbus/TryHackMeScripts/ 2> /dev/null
+
+# Crear el entorno virtual de Python
+  echo ""
+  echo "  Creando el entorno virtual de Python..."
+  echo ""
+  # Comprobar si el paquete python3-venv está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s python3-venv 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete python3-venv no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install python3-venv
+      echo ""
+    fi
+  cd $HOME/HackingTools/Modbus/TryHackMeScripts/
+  python3 -m venv PythonVirtualEnvironment
+  # Crear el mensaje para mostrar cuando se entra al entorno virtual
+    echo 'echo -e "\n  Activando el entorno virtual de Python para los scripts Modbus de TryHackMe... \n"' >> $HOME/HackingTools/Modbus/TryHackMeScripts/PythonVirtualEnvironment/bin/activate
+
+# Instalar los requisitos
+  echo ""
+  echo "  Instalando los requisitos..."
+  echo ""
+  source $HOME/HackingTools/Modbus/TryHackMeScripts/PythonVirtualEnvironment/bin/activate
+    pip3 install pymodbus==1.5.2
+    pip3 install six
+  deactivate
+
+# Descargar los scripts
+  echo ""
+  echo "  Descargando los scripts..."
+  echo ""
+  # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+      echo ""
+      sudo apt-get -y update
+      sudo apt-get -y install curl
+      echo ""
+    fi
+  cd $HOME/HackingTools/Modbus/TryHackMeScripts/
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
+  curl -sL -O 
