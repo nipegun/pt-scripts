2ed1090a1c7d43141c3fd99e54fbfe10ace4c44c
NiPeGun <nipegun@gmail.com>
2025-06-28 17:02:27 +0200

    Create Samba-UsuarioNoExistente-ListarOtrosUsuarios.sh

---
+++ 1-Reconnaissance/Samba-UsuarioNoExistente-ListarOtrosUsuarios.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para listar los usuarios de un servidor samba sabiendo el usuario y contraseña de un usuario válido
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-UsuarioNoExistente-ListarOtrosUsuarios.sh | bash -s 'IPServSamba' 'UsuarioNoExistente'
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-UsuarioNoExistente-ListarOtrosUsuarios.sh | sed 's-sudo--g' | bash -s 'IPServSamba' 'UsuarioNoExistente'
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-UsuarioNoExistente-ListarOtrosUsuarios.sh | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantParamEsperados=2
+
+# Comprobar que se hayan pasado la cantidad de parámetros correctos. Abortar el script si no.
+  if [ $# -ne $cCantParamEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo ""
+      if [[ "$0" == "bash" ]]; then
+        vNombreDelScript="script.sh"
+      else
+        vNombreDelScript="$0"
+      fi
+      echo "    $vNombreDelScript [IPServSamba] [UsuarioNoExistente]"
+      echo ""
+      echo "  Ejemplo:"
+      echo ""
+      echo "    $vNombreDelScript '10.10.100.206' 'x'"
+      echo ""
+      exit
+  fi
+
+vIPServSamba="$1"
+vUsuarioConocido="$2"
+vPassDelUsuario="$3"
+
+# Comprobar si el paquete crackmapexec está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s crackmapexec 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete crackmapexec no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install crackmapexec
+    echo ""
+  fi
+
+crackmapexec smb "$vIPServSamba" -u "$vUsuarioConocido" --users
+
