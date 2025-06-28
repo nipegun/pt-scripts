d66067a9e962618ed2328f7199c5b8a1c7ddb0d4
NiPeGun <nipegun@gmail.com>
2025-06-28 13:50:38 +0200

    Update Samba-ListarUsuarios.sh

--- 1-Reconnaissance/Samba-ListarUsuarios.sh
+++ 1-Reconnaissance/Samba-ListarUsuarios.sh
@@ -6,27 +6,32 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+# Script de NiPeGun para listar los usuarios de un servidor samba sabiendo el usuario y contraseña de un usuario válido
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL x | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-ListarUsuarios.sh | bash -s 'IPServSamba' 'UsuarioConocido' 'PassDelUsuario'
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL x | sed 's-sudo--g' | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-ListarUsuarios.sh | sed 's-sudo--g' | bash -s 'IPServSamba' 'UsuarioConocido' 'PassDelUsuario'
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL x | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/1-Reconnaissance/Samba-ListarUsuarios.sh | nano -
 # ----------
 
 vIPServSamba="$1"
 vUsuarioConocido="$2"
-vPassDeUsuario="$3"
+vPassDelUsuario="$3"
 
-crackmapexec smb "vIPServSamba=" -u "$vUsuarioConocido" -p "$vPassDeUsuario" --users
+# Comprobar si el paquete crackmapexec está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s crackmapexec 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete crackmapexec no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install crackmapexec
+    echo ""
+  fi
+
+crackmapexec smb "$vIPServSamba" -u "$vUsuarioConocido" -p "$vPassDelUsuario" --users
+
 
