b63e0501a1f6f43baead5afd075bf7ef17753b81
NiPeGun <nipegun@gmail.com>
2025-07-03 19:47:23 +0200

    Update and rename mbpoll-EscanearPLC.sh to mbpoll-EscanearPLC20.sh

--- 1-Reconnaissance/Modbus/mbpoll-EscanearPLC20.sh
+++ 1-Reconnaissance/Modbus/mbpoll-EscanearPLC20.sh
@@ -70,7 +70,7 @@ aSlaveIDsValidos=()
   echo ""
   for vSlaveID in $(seq 1 3); do
     echo -n "Probando Slave ID $vSlaveID... "
-    mbpoll -m tcp -t 4 -a $vSlaveID -r 0 -c 1 -0 -1 -o 1 $vIP > /dev/null 2>&1
+    mbpoll -m tcp -t 4 -a $vSlaveID -r 0 -c 1 -0 -1 -o 1 "$vIP" -p "$vPuerto" > /dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "✅ Responde"
     aSlaveIDsValidos+=($vSlaveID)
