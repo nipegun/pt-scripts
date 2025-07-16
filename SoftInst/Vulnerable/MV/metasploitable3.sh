5df44eac257f3113e44d7fdf6deee18fb3db8df3
NiPeGun <nipegun@gmail.com>
2025-07-16 03:28:50 +0200

    Update metasploitable3.sh

--- SoftInst/Vulnerable/MV/metasploitable3.sh
+++ SoftInst/Vulnerable/MV/metasploitable3.sh
@@ -7,7 +7,9 @@
   sudo gem install winrm winrm-fs winrm-elevated
 
 # Obtener el archivo vagrant
-  mkdir metasploitable3
-  cd metasploitable3
+  rm -rf ~/metasploitable3-workspace
+  mkdir ~/metasploitable3-workspace
+  cd ~/metasploitable3-workspace
   curl -O https://raw.githubusercontent.com/rapid7/metasploitable3/master/Vagrantfile
-  vagrant up
+  vagrant up --provider=virtualbox
+
