2d251202b50376bd87d7c979630424fd41e8104f
NiPeGun <nipegun@gmail.com>
2025-09-21 18:41:51 +0200

    Delete SoftInst/Vulnerable/MV/metasploitable3.ps1

--- SoftInst/Vulnerable/MV/metasploitable3.ps1
+++
@@ -1,4 +0,0 @@
-mkdir metasploitable3-workspace
-cd metasploitable3-workspace
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rapid7/metasploitable3/master/Vagrantfile" -OutFile "Vagrantfile"
-vagrant up
