7dbc51eba844a893efc709ad6404bf9474813a96
NiPeGun <nipegun@gmail.com>
2025-12-06 22:11:00 +0100

    Create Gobuster.sh

---
+++ 1-Enumerate/Web/Fuzzing/Gobuster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar y configurar xxxxxxxxx en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+vProtocolo="${1:-http}"
+vIP="${2:-127.0.0.1}"
+vPuerto="${3:-80}"
+vWordList="${4:-~/HackingTools/WordLists/EnTextoPlano/Packs/Debian/american-english-insane.txt}"
+vExcLenght="${5:-0}"
+
+# Comprobar si el paquete gobuster está instalado. Si no lo está, instalarlo.
+  if [[ $(dpkg-query -s gobuster 2>/dev/null | grep installed) == "" ]]; then
+    echo ""
+    echo -e "${cColorRojo}  El paquete gobuster no está instalado. Iniciando su instalación...${cFinColor}"
+    echo ""
+    sudo apt-get -y update
+    sudo apt-get -y install gobuster
+    echo ""
+  fi
+
+gobuster dir -u "$vProtocolo"://"$vIP":"$vPuerto" -w "$vWordList" -k  --exclude-length "$vExcLenght"
